45c753a2f8e6bfbd6e1a111a926fbd73329e7e37
licoris <klepehin.k.a@gmail.com>
2026-09-05 17:12:52 +0300

    первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+    // Вычисляем дискриминант
+    const d = b ** 2 - 4 * a * c;
+
+    if (d < 0) {
+        return []; // корней нет
+    } else if (d === 0) {
+        const root = -b / (2 * a);
+        return [root]; // один корень
+    } else {
+        const sqrtD = Math.sqrt(d);
+        const root1 = (-b + sqrtD) / (2 * a);
+        const root2 = (-b - sqrtD) / (2 * a);
+        return [root1, root2]; // два корня
+    }
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+// Примеры проверки:
+console.log(solveEquation(1, -3, 2)); // [2, 1]
+console.log(solveEquation(1, 2, 1));  // [-1]
+console.log(solveEquation(1, 0, 1));  // []
